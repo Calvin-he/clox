55d319e70579479507cba85a0a53b9e3bb982a91
Calvin He <xhe.com@gmail.com>
2020-09-22 10:44:37 +0000

    16. Scanning on Demand

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -94,9 +95,7 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
-{
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
